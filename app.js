9243358fcf1cb1ba55bf4248c48088917818e0d3
polinadotio <psoshnin@gmail.com>
2016-04-22 16:35:00 -0700

    add styling

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ angular.module('indieGram', [])
   '$scope',
   function($scope) {
     $scope.test = "hello world!";
+
     $scope.posts = [
       {title: 'post 1', upvotes: 5},
       {title: 'post 2', upvotes: 2},
@@ -10,11 +11,20 @@ angular.module('indieGram', [])
       {title: 'post 4', upvotes: 9},
       {title: 'post 5', upvotes: 4}
     ];
+
     $scope.addPost = function() {
       if (!$scope.title || $scope.title === '') {
         return;
       }
-      $scope.posts.push({title: $scope.title, upvotes: 0});
+      $scope.posts.push({
+        title: $scope.title, 
+        link: $scope.link,
+        upvotes: 0});
       $scope.title = "";
-    }
+      $scope.link = "";
+    };
+
+    $scope.incrementUpvotes = function(post) {
+      post.upvotes += 1;
+    };
   }]);
